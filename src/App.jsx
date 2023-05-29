b32d6310605f4ff60b9acde672676bdfbc167967
AndyKallian <andersonkallian@yahoo.com.br>
2023-05-29 17:04:03 -0300

    fixing centering div-components

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => (
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX}`}>
+    <div className={`bg-primary ${styles.flexCenter} ${styles.paddingX}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
